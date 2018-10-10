aa87eda75c6306ce681e4eea8f1e5e3e830da5ad
github username <github email address>
2018-10-09 21:55:55 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,35 @@
 // Code DigitalClicker Component Here
+import React, {Component} from 'react';
+
+export default class DigitalClicker extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      timesClicked : 0
+    }
+  }
+
+  increase = () => {
+    // console.log(this.state.timesClicked)
+    const newCount = this.state.timesClicked + 1
+    this.setState({
+      timesClicked : newCount
+    }, () => console.log(this.state.timesClicked))
+  }
+
+  render(){
+    return (
+      <button onClick={this.increase}>{this.state.timesClicked}</button>
+    )
+  }
+
+}
+
+
+
+
+// In the components/DigitalClicker.js file, create a DigitalClicker React component.
+// This component has an initial state property called timesClicked, which is initially defined as 0.
+// The component renders out a button with a label that shows the timesClicked value. This means that, at the start, your button should just say 0.
+// Whenever the button is clicked, update the state by incrementing the timesClicked by 1.
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,50 @@
 // Code YouTubeDebugger Component Here
+import React, {Component} from 'react';
+
+export default class YouTubeDebugger extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+       errors: [],
+       user: null,
+       settings: {
+         bitrate: 8,
+         video: {
+           resolution: '1080p'
+         }
+       }
+    }
+  }
+
+
+  changeBitRate = () => {
+    this.setState({
+      settings : {
+        ...this.state.settings,
+        bitrate: 12,
+      }
+    }, () => console.log(this.state))
+  }
+
+  changeRes = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+          video: {
+            resolution: '720p'
+          }
+        }
+      }, () => console.log(this.state))
+  }
+
+  render(){
+    return (
+      <div>
+      <button className="bitrate" onClick={this.changeBitRate}>Change Bit Rate</button>
+      <button className="resolution" onClick={this.changeRes}>Change Resolution</button>
+    </div>
+    )
+  }
+
+}
